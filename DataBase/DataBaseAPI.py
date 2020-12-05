990488eaa79a86ac3dcd4c66e0a80966558b2867
lala74 <laminhduc0704@gmail.com>
2020-12-05 19:00:12 +0100

    DataBaseAPI: finish all nessesary functions

--- DataBase/DataBaseAPI.py
+++ DataBase/DataBaseAPI.py
@@ -9,7 +9,14 @@ from sqlite3.dbapi2 import connect
 
 dataFileName = 'data.db'
 tableName = 'sensors'
-tableElements = '(date timestamp, sensor_id int, temp real, humid real)'
+
+date_name = 'date'
+sensor_id_name = 'sensor_id'
+temp_name = 'temperature'
+humid_name = 'humidiy'
+
+tableElements = '('+date_name+' timestamp, '+sensor_id_name + \
+    ' int, '+temp_name+' real, '+humid_name+' real)'
 
 
 class DataBaseAPI:
@@ -17,6 +24,46 @@ class DataBaseAPI:
     #     self.imgPath = imgPath
     conn = None
     cursor = None
+
+    def create_table(self):
+        self.__connect()
+        # '''CREATE TABLE IF NOT EXISTS sensors (date timestamp, sensor_id int, temp real, humid real)'''
+        cmd = 'CREATE TABLE IF NOT EXISTS ' + tableName + tableElements
+        self.__execute_cmd(cmd)
+
+    def insert_data(self, date, sensorId, temp, humid):
+        self.__connect()
+        # 'INSERT INTO sensors VALUES (123345, 3, 23.7, 0.8)'
+        cmd = 'INSERT INTO ' + tableName + \
+            ' VALUES (' + str(date) + ',' + str(sensorId) + \
+            ',' + str(temp)+',' + str(humid)+')'
+        self.__execute_cmd(cmd)
+
+    def load_data_by_sensor_id(self, sensor_id):
+        self.__connect()
+        # SELECT * FROM sensors WHERE sensor_id = 12
+        cmd = 'SELECT * FROM ' + tableName + \
+            ' WHERE ' + sensor_id_name + '=' + str(sensor_id)
+        value = self.cursor.execute(cmd)
+        return value
+
+    def print_database(self):
+        self.__connect()
+        for row in self.load_all_data():
+            print(row)
+
+    def load_all_data(self):
+        self.__connect()
+        # SELECT * FROM sensors
+        cmd = 'SELECT * FROM ' + tableName + ' ORDER BY ' + date_name
+        value = self.cursor.execute(cmd)
+        return value
+
+    def delete_database(self):
+        self.__connect()
+        # DELETE * FROM sensors
+        cmd = 'DELETE FROM ' + tableName
+        self.__execute_cmd(cmd)
 
     def __connect(self):
         self.conn = sqlite3.connect(dataFileName)
@@ -26,24 +73,7 @@ class DataBaseAPI:
         if self.conn != None:
             self.conn.close()
 
-    def create_table(self):
-        self.__connect()
-
-        # '''CREATE TABLE IF NOT EXISTS sensors (date timestamp, sensor_id int, temp real, humid real)'''
-        cmd = "CREATE TABLE IF NOT EXISTS " + tableName + tableElements
-        self.cursor.execute(cmd)
-        self.__close()
-
-    def insert_data(self, date, sensorId, temp, humid):
-        self.__connect()
-
-        # "INSERT INTO sensors VALUES (123345, 3, 23.7, 0.8)"
-        cmd = "INSERT INTO sensors VALUES (" + str(date) + "," + str(
-            sensorId) + "," + str(temp)+"," + str(humid)+")"
+    def __execute_cmd(self, cmd):
         self.cursor.execute(cmd)
         self.conn.commit()
-
         self.__close()
-
-    # def delete_database():
-        
